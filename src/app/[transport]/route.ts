3d7829207207160dc13be758ef1da7eba6258751
Eugene Maestrado <eugene.m@qbe.com>
2025-06-27 17:36:58 +0930

    adds route

---
+++ src/app/[transport]/route.ts
@@ -0,0 +1,36 @@
+import { createMcpHandler } from "@vercel/mcp-adapter";
+import { z } from "zod";
+
+const handler = createMcpHandler(
+  async (server) => {
+    server.tool(
+      "echo",
+      "description",
+      {
+        message: z.string(),
+      },
+      async ({ message }) => ({
+        content: [{ type: "text", text: `Tool echo: ${message}` }],
+      })
+    );
+  },
+  {
+    capabilities: {
+      tools: {
+        echo: {
+          description: "Echo a message",
+        },
+      },
+    },
+  },
+  {
+    redisUrl: process.env.REDIS_URL,
+    sseEndpoint: "/sse",
+    streamableHttpEndpoint: "/mcp",
+    basePath: "",
+    verboseLogs: true,
+    maxDuration: 60,
+  }
+);
+
+export { handler as GET, handler as POST, handler as DELETE };
