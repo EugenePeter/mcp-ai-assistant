fdbfb3f974a955dab946671a210a56711b79f7d8
Eugene Maestrado <eugene.m@qbe.com>
2025-06-27 18:29:03 +0930

    test change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>hello welcome</h1>
+      <h1>hello there</h1>
     </div>
   );
 }
